5efb97ac274c6d61eaa819b86562d5b3490e7767
Your Name <you@example.com>
2024-02-13 01:16:14 -0600

    fixed builds 2

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const socket = require("socket.io");
 
 const app = express();
-const server = app.listen(4000, function () {
+const server = app.listen(3000, function () {
   console.log("Listening to requests on port 4000");
 });
 
